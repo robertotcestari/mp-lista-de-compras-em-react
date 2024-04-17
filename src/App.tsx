70398bd4840176d912ac510b3734cd0df5854f23
Roberto Cestari <robertotcestari@gmail.com>
2024-04-17 10:51:53 -0300

    fix add line through

--- src/App.tsx
+++ src/App.tsx
@@ -76,8 +76,8 @@ function App() {
         <article className="flex w-full gap-4">
           <img src={done} alt="#" />
           <div className="flex-1">
-            <p>Leite</p>
-            <p className="text-sm text-slate-400">3 Caixas</p>
+            <p className="line-through text-slate-400">Leite</p>
+            <p className="text-sm line-through text-slate-400">3 Caixas</p>
           </div>
           <img
             src={trash}
@@ -89,8 +89,8 @@ function App() {
         <article className="flex w-full gap-4">
           <img src={done} alt="#" />
           <div className="flex-1">
-            <p>Maçã</p>
-            <p className="text-sm text-slate-400">500g</p>
+            <p className="line-through text-slate-400">Maçã</p>
+            <p className="text-sm line-through text-slate-400">500g</p>
           </div>
           <img
             src={trash}
